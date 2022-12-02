5d7ad79682639ee8f96af92c277b61f4faddec85
ariessa-deriv <>
2022-12-02 20:20:04 +0800

    Add sse listener and setup redis for storing prices data

--- app/index.js
+++ app/index.js
@@ -4,9 +4,44 @@ const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./graphql/schema");
 const Cookies = require("cookies");
+const EventSource = require("eventsource");
+const cacheClient = require("./lib/cache");
 
 // Load .env file contents into process.env
 dotenv.config();
+
+const sse = new EventSource("http://143.198.218.123:5000");
+sse.onmessage = async (e) => {
+  try {
+    const data = JSON.parse(e.data);
+
+    // console.log("data: ", data);
+
+    // Store the prices
+    await cacheClient
+      .multi()
+      .lpush(
+        "historical_prices",
+        JSON.stringify({
+          boom_100: data.current_boom_100_price,
+          crash_100: data.current_crash_100_price,
+          volatility_10: data.current_vol_10_price,
+          volatility_25: data.current_vol_25_price,
+          time: data.time_asia_kuala_lumpur,
+        })
+      )
+
+      // Trim the prices list to 60 entries only
+      .ltrim("historical_prices", 0, 59)
+      .exec();
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+sse.onerror = (e) => {
+  console.log("Error:", e.message);
+};
 
 var app = express();
 
