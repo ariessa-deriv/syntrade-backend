56ff954af13d67298c582aa5f75819b6306842b0
ariessa-deriv <>
2022-12-02 20:21:49 +0800

    Uncomment content of cache.js file

--- app/lib/cache.js
+++ app/lib/cache.js
@@ -1,19 +1,17 @@
-// const Redis = require("ioredis");
-// const dotenv = require("dotenv");
+const Redis = require("ioredis");
+const dotenv = require("dotenv");
 
-// // Load .env file contents into process.env
-// dotenv.config();
+// Load .env file contents into process.env
+dotenv.config();
 
-// const cacheClient = new Redis(
-//   `redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@syntrade-cache:${process.env.REDIS_PORT}`
-// );
+const cacheClient = new Redis(
+  `redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@syntrade-cache:${process.env.REDIS_PORT}`
+);
 
-// cacheClient.on("error", (err) => console.log("Redis Client Error", err));
+cacheClient.on("error", (err) => console.log("Redis Client Error", err));
 
-// cacheClient.connect();
+cacheClient.on("connect", function () {
+  console.log("Redis Client Connected!");
+});
 
-// cacheClient.on("connect", function () {
-//   console.log("Redis Client Connected!");
-// });
-
-// module.exports = cacheClient;
+module.exports = cacheClient;
