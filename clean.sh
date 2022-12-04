369571e1bbfbef0db35bb8a8a784ba2d0f4876ea
ariessa-deriv <>
2022-12-04 19:05:55 +0800

    Update cleaning script

--- clean.sh
+++ clean.sh
@@ -9,8 +9,14 @@ docker rm $(docker ps -a -q) || true
 # Remove all Docker images
 docker rmi $(docker images -q) || true
 
-# Remove postgres and redis folders
+# Delete postgres and redis folders
 sudo rm -rf postgres redis || true
 
 # Check which process is running on port 5432 and kill the process by PID
 sudo kill -9 $(sudo lsof -i :5432 | awk 'NR==2{print $2}') || true
+
+# Delete package-lock.json file
+sudo rm -f app/package-lock.json || true
+
+# Delete node_modules folder
+sudo rm -rf app/node_modules || true
