48940ef5e413792b597889d045145ae9f4798a87
ariessa-deriv <>
2022-12-05 10:14:21 +0800

    Throw generic errors

--- app/lib/input_validations.js
+++ app/lib/input_validations.js
@@ -2,20 +2,17 @@
 const checkEmailValidity = (email) => {
   // Check if email is empty or not
   if (!email) {
-    console.log("Error: Email cannot be empty");
-    throw new Error("Email cannot be empty");
+    throw new Error("Invalid email");
   }
 
   // Check if email contains any whitespace or not
   if (/\s/.test(email)) {
-    console.log("Error: Email cannot contain spaces");
-    throw new Error("Email cannot contain spaces");
+    throw new Error("Invalid email");
   }
 
   // Check if email is in correct format or not
   if (!/\S+@\S+\.\S+/.test(email)) {
-    console.log("Error: Invalid email format");
-    throw new Error("Invalid email format");
+    throw new Error("Invalid email");
   }
   return true;
 };
@@ -31,30 +28,22 @@ const checkPasswordValidity = (password) => {
 
   // Check if password is empty or not
   if (!password) {
-    console.log("Error: Password cannot be empty");
-    throw new Error("Password cannot be empty");
+    throw new Error("Invalid password");
   }
 
   // Check if length of password is between 8-12 characters or not
   if (password.length < 8 || password.length > 12) {
-    console.log("Error: Password must be between 8-12 characters");
-    throw new Error("Password must be between 8-12 characters");
+    throw new Error("Invalid password");
   }
 
   // Check if password contains any whitespace or not
   if (/\s/.test(password)) {
-    console.log("Error: Password cannot contain spaces");
-    throw new Error("Password cannot contain spaces");
+    throw new Error("Invalid password");
   }
 
   // Check if password have at least 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character
   if (!pattern.test(password)) {
-    console.log(
-      "Error: Password must have at least 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character"
-    );
-    throw new Error(
-      "Password must have at least 1 uppercase letter, 1 lowercase letter, 1 digit and 1 special character"
-    );
+    throw new Error("Invalid password");
   }
 
   return true;
